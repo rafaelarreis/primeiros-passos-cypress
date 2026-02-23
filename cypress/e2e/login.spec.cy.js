56dd3605c2a2638015315a60786953e0ae5aa6b8
Rafaela Reis <rafaelareis7@hotmail.com>
2026-02-23 22:05:09 +0000

    added selectors list for login spec

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,29 @@
+describe("login HRM test", () => {
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: ".oxd-button",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    wrongCredentialAlert: ".oxd-alert",
+  };
+
+  it("´login - success", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+    cy.get(selectorsList.usernameField).type("Admin");
+    cy.get(selectorsList.passwordField).type("admin123");
+    cy.get(selectorsList.loginButton).click();
+    cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
+    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard");
+  });
+  it("´login - fail", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+    cy.get(selectorsList.usernameField).type("test");
+    cy.get(selectorsList.passwordField).type("test");
+    cy.get(selectorsList.loginButton).click();
+    cy.get(selectorsList.wrongCredentialAlert);
+  });
+});
