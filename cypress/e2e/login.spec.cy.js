0b7385d0885685c898d552be48025d6af5b9c721
Rafaela Reis <rafaelareis7@hotmail.com>
2026-02-24 13:48:18 +0000

    usando fixtures

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userData from "../fixtures/users/userData.json";
+
 describe("login HRM test", () => {
   const selectorsList = {
     usernameField: "[name='username']",
@@ -11,8 +13,8 @@ describe("login HRM test", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
-    cy.get(selectorsList.usernameField).type("Admin");
-    cy.get(selectorsList.passwordField).type("admin123");
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
     cy.get(selectorsList.loginButton).click();
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
     cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard");
@@ -21,8 +23,8 @@ describe("login HRM test", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
-    cy.get(selectorsList.usernameField).type("test");
-    cy.get(selectorsList.passwordField).type("test");
+    cy.get(selectorsList.usernameField).type(userData.userFail.username);
+    cy.get(selectorsList.passwordField).type(userData.userFail.password);
     cy.get(selectorsList.loginButton).click();
     cy.get(selectorsList.wrongCredentialAlert);
   });
